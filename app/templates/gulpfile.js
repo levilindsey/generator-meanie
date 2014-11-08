1b5873f32943f4be176ebc5976e566124c6cba0a
Levi Lindsey <levisl176@gmail.com>
2014-11-08 10:29:28 -0800

    Adds additional gulp watches.

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,9 +3,11 @@ var glob = require('glob');
 
 function loadTasks(includes) {
   includes
+      // Expand the glob to get an array of the actual file paths
       .reduce(function (paths, include) {
         return paths.concat(glob.sync(include));
       }, [])
+      // Register each task with gulp
       .forEach(function (path) {
         require(path);
       });
